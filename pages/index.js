1d1d0e0e891680ffa7ac281395a7a0cf39630037
yashG0 <yashgaurkar0@gmail.com>
2023-03-30 19:42:00 +0530

    Added new feature

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import Hero from '@/components/Hero';
+import { Parallax } from 'react-parallax';
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
@@ -14,110 +16,36 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+
+      <Parallax bgImage={"img.jpg"} bgImageAlt="background image" strength={200} >
+        <div className="content h-screen">
+          <div className="text-content absolute top-1/2 left-1/2 py-1 px-4 -translate-x-1/2 -translate-y-1/2 bg-gray-600 text-white rounded-full text-xl">
+          My Parallax - 0
           </div>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+      </Parallax>
+      <Parallax bgImage={"img2.jpg"} bgImageAlt="background image" strength={200} >
+        <div className="content h-screen">
+          <div className="text-content absolute top-1/2 left-1/2 py-1 px-4 -translate-x-1/2 -translate-y-1/2 bg-gray-600 text-white rounded-full text-xl">
+            My Parallax - 1
           </div>
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+      </Parallax>
+      <Parallax bgImage={"img3.jpg"} bgImageAlt="background image" strength={200} >
+        <div className="content h-screen">
+          <div className="text-content absolute top-1/2 left-1/2 py-1 px-4 -translate-x-1/2 -translate-y-1/2 bg-gray-600 text-white rounded-full text-xl">
+          My Parallax - 2
+          </div>
         </div>
-      </main>
+      </Parallax>
+      <Parallax bgImage={"img4.jpg"} bgImageAlt="background image" strength={200} >
+        <div className="content h-screen">
+          <div className="text-content absolute top-1/2 left-1/2 py-1 px-4 -translate-x-1/2 -translate-y-1/2 bg-gray-600 text-white rounded-full text-xl">
+          My Parallax - 3
+          </div>
+        </div>
+      </Parallax>
+      <div className='h-screen'></div>
     </>
   )
 }
